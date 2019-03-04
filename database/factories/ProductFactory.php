7d83f0b4b9a37349662ad37298ee86bde0fa5e05
Rockon <rockon3334@hotmail.com>
2019-03-04 14:52:35 +0600

    database seeding

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,5 +5,10 @@ use Faker\Generator as Faker;
 $factory->define(App\model\product::class, function (Faker $faker) {
     return [
         //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(100,1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(20,40)
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,16 @@
 <?php
 
 use Faker\Generator as Faker;
+use app\model\product;
 
 $factory->define(App\model\review::class, function (Faker $faker) {
     return [
         //
+        'product_id' => function(){
+            return Product::all()->random();
+        },
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0,5)
     ];
 });
